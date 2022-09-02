740a3b174d4a6ec1d9b00e24c7563706ed3d7854
zunayed-arp <zunayed.arp@gmail.com>
2022-09-02 14:46:07 +0600

    reafactoring: base settings

--- socialnetwork/settings/base.py
+++ socialnetwork/settings/base.py
@@ -14,13 +14,13 @@ import logging.config
 from django.utils.log import DEFAULT_LOGGING
 import environ
 from pathlib import Path
-
 import os
 
+env = environ.Env(DEBUG=(bool, False))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-env = environ.Env(DEBUG=(bool, False))
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
