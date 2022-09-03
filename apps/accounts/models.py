dfb2dde3d9fb3af144c8b6f9f3ed4b4b7bc6dbec
zunayed-arp <zunayed.arp@gmail.com>
2022-09-04 02:06:09 +0600

    custom user model init

---
+++ apps/accounts/models.py
@@ -0,0 +1,33 @@
+from pyexpat import model
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    username = models.CharField(
+        "username",
+        max_length=50,
+        unique=True,
+        help_text="Required. 50 characters or fewer. Letters, digits and @/./+/-/_only.",
+        error_messages={
+            "unique": "A user with that username already exists.",
+        },
+    )
+    email = models.EmailField(
+        unique=True,
+        blank=False,
+        error_messages={"unique": "A user with that email already exists."},
+    )
+
+    gender = models.CharField(max_length=20)
+    status = models.BooleanField(default=False)
+    about = models.TextField(blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __unicode__(self):
+        return self.email
+
+    def __str__(self) -> str:
+        return self.get_full_name()
